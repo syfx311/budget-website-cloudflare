7ae9659cd978a26c17418add197e0d9b0091697d
syfx311 <tramiecha@gmail.com>
2026-06-17 01:33:28 -0700

    Update open-next.config.ts

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,5 +1,25 @@
-import type { CloudflareConfig } from "@opennextjs/cloudflare";
-
-const config: CloudflareConfig = {};
-
+const config = {
+  default: {
+    override: {
+      wrapper: "cloudflare-node",
+      converter: "edge",
+      proxyExternalRequest: "fetch",
+      incrementalCache: "dummy",
+      tagCache: "dummy",
+      queue: "dummy"
+    }
+  },
+  edgeExternals: ["node:crypto"],
+  middleware: {
+    external: true,
+    override: {
+      wrapper: "cloudflare-edge",
+      converter: "edge",
+      proxyExternalRequest: "fetch",
+      incrementalCache: "dummy",
+      tagCache: "dummy",
+      queue: "dummy"
+    }
+  }
+};
 export default config;
